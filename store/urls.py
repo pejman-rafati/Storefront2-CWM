c1827b88ce4046ab87897811d132a9d8ebfc3c89
pejman rafati <pejman.rafati@gmail.com>
2024-03-10 17:14:19 +0330

    lesson 10 - simple filtering

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from rest_framework_nested import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet)
+router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
 router.register('reviews', views.ReviewViewSet)
 

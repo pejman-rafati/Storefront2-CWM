a9d07730869ca101327040779352d2aff67ba0ad
pejman rafati <pejman.rafati@gmail.com>
2024-03-10 17:04:54 +0330

    lesson 9 - adding NestedRouters for review

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,11 @@
-from rest_framework.routers import SimpleRouter, DefaultRouter
+from rest_framework_nested import routers
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 router.register('reviews', views.ReviewViewSet)
-urlpatterns = router.urls
+
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+urlpatterns = router.urls + products_router.urls

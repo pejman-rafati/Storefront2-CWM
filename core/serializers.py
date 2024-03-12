aee945b9845c9cc3fae393eb9eb9442f81690cdc
pejman rafati <pejman.rafati@gmail.com>
2024-03-12 15:56:00 +0330

    sec6-less5 - customer serializer & viewset created to build profile api

--- core/serializers.py
+++ core/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'birth_date']
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'birth_date', ]

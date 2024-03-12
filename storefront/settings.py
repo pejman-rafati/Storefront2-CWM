695b0e24324aee5b1b1bf2a22cde5d1fd0612aaf
pejman rafati <pejman.rafati@gmail.com>
2024-03-12 15:37:37 +0330

    sec6-less3-4 - adding djoser & authentication endpoint

--- storefront/settings.py
+++ storefront/settings.py
@@ -149,3 +149,8 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
 }
 AUTH_USER_MODEL = 'core.User'
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'core.serializers.UserCreateSerializer',
+    }
+}
